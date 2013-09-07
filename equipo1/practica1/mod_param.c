1c7a97070887871dea0c80f9d696b7c62cd4b8c9
Robert <rovert727@hotmail.com>
2013-09-07 12:59:29 -0500

    update

--- equipo1/practica1/mod_param.c
+++ equipo1/practica1/mod_param.c
@@ -11,12 +11,16 @@ return 0;
 
 static void __exit hello_exit(void)
 {
-pr_info("%i\n", factorial(module_param));
+/* Validando que los valores del parametro esten en un rango aceptable */
+if (module_param < 0 || module_param > 50)
+	pr_info("Parametro invalido, proporcione uno entre el rango 0-50");
+else
+	pr_info("%i\n", factorial(module_param));
 }
 
 module_init(hello_init);
 module_exit(hello_exit);
 module_param(module_param, int, (S_IWUSR|S_IRUSR));
 MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Robert");
+MODULE_AUTHOR("Robert y Lasa");
 MODULE_DESCRIPTION("Practica1");
--- equipo1/practica1/myfuncs.c
+++ equipo1/practica1/myfuncs.c
@@ -1,5 +1,7 @@
 #include "myfuncs.h"
-
+/* Funcion factorial, recibe un parametro mayor a 0 y menor o igual a 50
+   como precondicion y calcula el factorial de ese numero de manera
+   recursiva. */
 int factorial(int n)
 {
 if (n < 2)
