1585651a4338468cbc5b78ec653a184c0b8ad926
Ivan Gomez Castellanos <ivan.gomez@itesm.mx>
2013-09-21 09:31:25 -0700

    EXAMPLE: Implemented the read and write functions

--- ejemplos/char_drv/char_drv.c
+++ ejemplos/char_drv/char_drv.c
@@ -1,117 +1,209 @@
-#include <linux/module.h>
-#include <linux/fs.h>
-#include <linux/cdev.h>
-#include <linux/device.h>
+#include <linux/module.h>	/* Dynamic loading of modules into the kernel */
+#include <linux/fs.h>		/* file defs of important data structures */
+#include <linux/cdev.h>		/* char devices structure and aux functions */
+#include <linux/device.h>	/* generic, centralized driver model */
+#include <linux/uaccess.h>	/* for accessing user-space */
 
-#define DEVICE_NAME	"MY_DEV"
+#define KBUFF_MAX_SIZE	1024	/* size of the kernel side buffer */
+#define DEVICE_NAME 	"mydev"
+
 
 static unsigned major = 0;			/* major number */
-static struct cdev my_dev;			/* to register with kernel */
+static struct cdev mydev;			/* to register with kernel */
 static struct class *mydev_class = NULL;	/* to register with sysfs */
 static struct device *mydev_device;		/* to register with sysfs */
 
-static ssize_t my_read(struct file *filp, char __user *buf, 
-					size_t nbuf, loff_t *offs)
+static char kbuffer[KBUFF_MAX_SIZE];		/* kernel side buffer */
+static unsigned long kbuffer_size = 0;		/* kernel buffer length */
+
+/*
+ * mydev_read()
+ * This function implements the read function in the file operation structure.
+ */
+static ssize_t mydev_read(struct file *filp, char __user *buffer, size_t nbuf,
+								loff_t *offset)
 {
-	printk(KERN_INFO "%s:%d\n", __func__, __LINE__);
+	int ret = 0;
+	pr_info("[%d] %s\n", __LINE__, __func__);
+	pr_info("[%d] nbuf = %d, offset = %d", __LINE__, (int)nbuf, (int)*offset);
+	/* If offset is greater that size of kbuffer, there is nothing to copy */
+	if (*offset >= kbuffer_size) {
+		pr_info("[%d] Offset greater or equal than kbuffer size: %d >= %ld", __LINE__, (int)*offset, kbuffer_size);
+		goto out;
+	}
+
+	/* Check for maximum size of kernel buffer */
+	if ((nbuf + *offset) > kbuffer_size)
+		nbuf = kbuffer_size - *offset;
+
+	/* fill the buffer, return the buffer size */
+	pr_info("[%d] Copying Buffer from kernel to user space...\n", __LINE__);
+	ret = copy_to_user(buffer, &kbuffer[*offset], nbuf);
+	if (ret) {
+		pr_info("copy_to_user failed: 0x%x", ret);
+		ret = -EFAULT;
+		goto out;
+	}
+
+	*offset += nbuf;
+	ret = nbuf;
+
+out:
+	pr_info("[%d] nbuf = %d, offset = %d", __LINE__, (int)nbuf, (int)*offset);
+	return ret;
+}
+
+/*
+ * mydev_write()
+ * This function implements the write function in the file operation structure.
+ */
+static ssize_t mydev_write(struct file *filp, const char __user *buffer,
+						size_t nbuf, loff_t *offset)
+{
+	int ret = 0;
+
+	pr_info("[%d] nbuf = %d, offset = %d", __LINE__, (int)nbuf, (int)*offset);
+
+	if (*offset >= KBUFF_MAX_SIZE) {
+		pr_info("[%d] Offset greater or equal than kbuffer size: %d >= %ld", __LINE__, (int)*offset, kbuffer_size);
+		goto out;
+	}
+
+	/* Check for maximum size of kernel buffer */
+	if ((nbuf + *offset) > KBUFF_MAX_SIZE)
+		nbuf = KBUFF_MAX_SIZE - *offset;
+
+	/* fill the buffer, return the buffer size */
+	pr_info("[%d] Copying Buffer from user to kernel space...\n", __LINE__);
+	ret = copy_from_user(&kbuffer[*offset], buffer, nbuf);
+	if (ret) {
+		pr_info("copy_from_user failed: 0x%x", ret);
+		ret = -EFAULT;
+		goto out;
+	}
+
+	*offset += nbuf;
+	kbuffer_size = *offset;
+	ret = nbuf;
+
+out:
+	pr_info("[%d] nbuf = %d, offset = %d", __LINE__, (int)nbuf, (int)*offset);
+	return ret;
+}
+
+/*
+ * Function called when the user side application opens a handle to mydev driver
+ * by calling the open() function.
+ */
+
+static int mydev_open(struct inode *ip, struct file *filp)
+{
+
+	pr_info("Driver's open function was called\n");
+
 	return 0;
 }
 
-static ssize_t my_write(struct file *filp, const char __user *buf,
-					size_t nbuf, loff_t *offs)
-{
-	printk(KERN_INFO "%s:%d\n", __func__, __LINE__);
-	return nbuf;
-}
+/*
+ * Function called when the user side application closes a handle to mydev driver
+ * by calling the close() function.
+ */
 
-static int my_open(struct inode *ip, struct file *filp)
+static int mydev_close(struct inode *ip, struct file *filp)
 {
-	printk(KERN_INFO "%s:%d\n", __func__, __LINE__);
-	return 0;
-}
 
-static int my_close(struct inode *ip, struct file *filp)
-{
-	printk(KERN_INFO "%s:%d\n", __func__, __LINE__);
+	pr_info("Driver's close function was called\n");
+
 	return 0;
 }
 
 
+/*
+ * File operation structure
+ */
 static const struct file_operations mydev_fops = {
+	.open = mydev_open,
+	.release = mydev_close,
 	.owner = THIS_MODULE,
-	.read = my_read,
-	.write = my_write,
-	.open = my_open,
-	.release = my_close,
+	.read = mydev_read,
+	.write = mydev_write
 };
 
-static int __init mymodule_init (void)
+/*
+ * init my_calc_init()
+ * Module init function. It is called when insmod is executed.
+ */
+static int __init my_calc_init (void)
 {
-	dev_t dev_id;
 	int ret;
+	dev_t devid;
 
-	/* Allocate major/minor numbers */
-	ret = alloc_chrdev_region(&dev_id, 0, 1, DEVICE_NAME);
+	/* Dynamic allocation of MAJOR and MINOR numbers */
+	ret = alloc_chrdev_region(&devid, 0, 1, DEVICE_NAME);
+
 	if (ret) {
-		printk(KERN_INFO "Error: Failed registering major number\n");
-		return -1;
+		printk("Error: Failed registering major number\n");
+		return ret;
 	}
-	/* save MAJOR number */
-	major = MAJOR(dev_id);
 
-	/* Initialize cdev structure and register it with the kernel*/
-	cdev_init(&my_dev, &mydev_fops);
-	my_dev.owner = THIS_MODULE;
+	/* save MAJOR number */
+	major = MAJOR(devid);
+
+	/* Initalize the cdev structure */
+	cdev_init(&mydev, &mydev_fops);
+
 	/* Register the char device with the kernel */
-	ret = cdev_add(&my_dev, dev_id, 1);
+	ret = cdev_add(&mydev, devid, 1);
 	if (ret) {
-		printk(KERN_INFO "Error: Failed registering with the kernel\n");
-		unregister_chrdev_region(dev_id, 1);
-		return -1;
+		printk("Error: Failed registering with the kernel\n");
+		unregister_chrdev_region(devid, 1);
+		return ret;
 	}
 
 	/* Create a class and register with the sysfs. (Failure is not fatal) */
 	mydev_class = class_create(THIS_MODULE, DEVICE_NAME);
 	if (IS_ERR(mydev_class)) {
-		printk(KERN_INFO "class_create() failed: %ld\n",
-						PTR_ERR(mydev_class));
+		printk("class_create() failed: %ld\n", PTR_ERR(mydev_class));
 		mydev_class = NULL;
 	} else {
 		/* Register device with sysfs (creates device node in /dev) */
-		mydev_device = device_create(mydev_class, NULL, dev_id,
-							NULL, DEVICE_NAME"0");
+		mydev_device = device_create(mydev_class, NULL, devid, NULL, DEVICE_NAME"0");
 		if (IS_ERR(mydev_device)) {
-			printk(KERN_INFO "device_create() failed: %ld\n",
-						PTR_ERR(mydev_device));
+			printk("device_create() failed: %ld\n", PTR_ERR(mydev_device));
 			mydev_device = NULL;
 		}
 	}
 
-	printk(KERN_INFO "Driver \"%s\" loaded...\n", DEVICE_NAME);
+	printk("Driver \"%s\" loaded...\n", DEVICE_NAME);
 	return 0;
-}  
+}
 
-static void __exit mymodule_exit(void) 
+/*
+ * init my_calc_init()
+ * Module exit function. It is called when rmmod is executed.
+ */
+static void __exit my_calc_exit(void) 
 {
-	/* Deregister services from kernel */
-	cdev_del(&my_dev);
+	/* Deregister char device from kernel */
+	cdev_del(&mydev);
 
-	/* Release major/minor numbers */
+	/* Release MAJOR and MINOR numbers */
 	unregister_chrdev_region(MKDEV(major, 0), 1);
 
-	/* Undone everything initialized in init function */
+	/* Deregister device from sysfs */
 	if (mydev_class != NULL) {
 		device_destroy(mydev_class, MKDEV(major, 0));
 		class_destroy(mydev_class);
 	}
 
-	printk(KERN_INFO "Driver \"%s\" unloaded...\n", DEVICE_NAME);
-} 
+	printk("Driver \"%s\" unloaded...\n", DEVICE_NAME);
+}  
 
-module_init(mymodule_init);
-module_exit(mymodule_exit);
+module_init(my_calc_init);
+module_exit(my_calc_exit);
 
-MODULE_AUTHOR("Ivan Gomez Castellanos <ivan.gomez@itesm.mx>"); 
+MODULE_AUTHOR("Ivan Gomez Castellanos"); 
 MODULE_LICENSE("GPL"); 
-MODULE_DESCRIPTION("Char driver skeleton");
+MODULE_DESCRIPTION("Character Driver.");
+
 
